927fb632764bb4480277051aa49ac5adb8f41ded
Mohammed Alkutrani <mohammedalkutrani@gmail.com>
2020-01-14 13:06:38 +0200

    adding boolean

---
+++ src/Rules/Boolean.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Validator\Rules;
+
+use Validator\RuleInterface;
+
+class Boolean implements RuleInterface
+{
+    /**
+     * Check if the value is passed.
+     * 
+     * @param mix $value
+     * @return boolean
+     */
+    public function isValid($value) : bool
+    {
+        if (!\is_bool($value)) { 
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get error message.
+     * 
+     * @param mix $attribute
+     * @return string
+     */
+    public function getMessage($attribute) : string
+    {
+        return "the $attribute should be a boolean";
+    }
+}
