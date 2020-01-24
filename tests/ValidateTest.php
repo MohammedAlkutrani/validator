09949d6bfa46f12bb2df96952fc181122da417fe
Mohammed Alkutrani <mohammedalkutrani@gmail.com>
2020-01-24 17:42:49 +0200

    formating

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -9,17 +9,7 @@ class ValidateTest extends TestCase
 {
     public function testThrowException()
     {
-<<<<<<< HEAD
-        $v = new Validate(
-                ['name'=>'mohammed','age' => 15] ,[ [Rules::NUMBER],
-               [Rules::NUMBER],
-            ]);
-            var_dump($v);
-            die();
-        $this->expectException(ValidationException::class);
-=======
         $v = true;
         $v->assertEquals(false,$v);
->>>>>>> 07752b0dfd4dc0824301cdae5ffb1d28c51a67c6
     }
 }
