b3eeebc97907b8585fd058b398112133fa40cecd
MohammedAlkutrani <mohammedalkutrani@gmail.com>
2020-01-27 10:54:19 +0000

    Apply fixes from StyleCI

--- src/Validate.php
+++ src/Validate.php
@@ -21,7 +21,7 @@ class Validate
 
     /**
      * wrapping the validation.
-     * 
+     *
      * @param array $data
      * @param array $rules
      */
@@ -55,10 +55,10 @@ class Validate
 
     /**
      * Determine if the data is passed.
-     * 
+     *
      * @return bool
      */
-    public function isPassed() : bool
+    public function isPassed(): bool
     {
         if ($this->messages) {
             return false;
@@ -69,23 +69,23 @@ class Validate
 
     /**
      * Get the error messages.
-     * 
+     *
      * @return array $messages
      */
-    public function getMessages() : array
+    public function getMessages(): array
     {
         return $this->messages;
     }
 
     /**
      * Fitching the rules for the field.
-     * 
+     *
      * @param array $rules
-     * @param mix $rules
-     * 
+     * @param mix   $rules
+     *
      * @return void
      */
-    private function ruleFitcher(array $rules, $attribute) : void
+    private function ruleFitcher(array $rules, $attribute): void
     {
         foreach ($rules as $rule) {
             $this->ruleChecker(new $rule(), $attribute, $this->data[$attribute]);
@@ -94,14 +94,14 @@ class Validate
 
     /**
      * Fill the message if the rule not valid.
-     * 
+     *
      * @param Validator\RuleInterface $rule
-     * @param mix $attribute
-     * @param mix $value
-     * 
+     * @param mix                     $attribute
+     * @param mix                     $value
+     *
      * @return void
      */
-    private function ruleChecker(RuleInterface $rule, $attribute, $value) : void
+    private function ruleChecker(RuleInterface $rule, $attribute, $value): void
     {
         if (!$rule->isValid($value)) {
             $this->messages[$attribute][] = $rule->getMessage($attribute);
