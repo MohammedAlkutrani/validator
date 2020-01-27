2c9906886a92839ffd66cbdbea524d7540d27a08
Mohammed Alkutrani <mohammedalkutrani@gmail.com>
2020-01-27 12:01:03 +0200

    facades handle the injection

--- src/Facade/Validation.php
+++ src/Facade/Validation.php
@@ -2,13 +2,14 @@
 
 namespace Validator\Facade;
 
+use Validator\Parser;
 use Validator\Validate;
 
 class Validation
 {
     public static function __callStatic($name, $arguments)
     {
-        $v = new Validate();
+        $v = new Validate(new Parser);
 
         return $v->{$name}(...$arguments);
     }
