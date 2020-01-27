7e8b5899f88385a0b468730854da99f8ca51958e
Mohammed Alkutrani <mohammedalkutrani@gmail.com>
2020-01-27 11:59:09 +0200

    parseing the data

--- src/Parser.php
+++ src/Parser.php
@@ -6,10 +6,10 @@ class Parser
 {
     /**
      * Determine if the string has char.
-     *  
+     *
      * @param string $haystack
      * @param string $needle
-     * 
+     *
      * @return bool
      */
     public function has($haystack, string $needles) : bool
@@ -24,41 +24,41 @@ class Parser
 
     /**
      * Getting argument from string.
-     * 
+     *
      * @param string $data
-     * 
+     *
      * @return array
      */
     public function getArguments(string $data) : array
     {
-        $toArray = explode('|',$data);
+        $toArray = explode('|', $data);
         $toArray = $this->cleanArguments($toArray);
         return $toArray;
     }
 
     /**
      * Removing the class name from argument.
-     * 
+     *
      * @param array $arguments
-     * 
+     *
      * @return array
      */
     public function cleanArguments(array $arguments) : array
     {
         array_shift($arguments);
-        return $arguments; 
+        return $arguments;
     }
 
     /**
      * Get the class/rule name.
-     * 
+     *
      * @param string $rule.
-     * 
+     *
      * @return string
      */
     public function getRule(string $rule) : string
     {
-        $toArray = explode('|',$rule);
+        $toArray = explode('|', $rule);
         return $toArray[0];
     }
-
+}
