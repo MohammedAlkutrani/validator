c6d6d347145f4682f755570c939abf59cc90cd5f
Mohammed Alkutrani <mohammedalkutrani@gmail.com>
2020-01-27 12:05:11 +0200

    adding max value

--- src/Rules.php
+++ src/Rules.php
@@ -7,7 +7,8 @@ class Rules
     /**
      * @var
      */
-    const NUMBER = 'Validator\Rules\Number';
-    const BOOLEAN = 'Validator\Rules\Boolean';
-    const EMAIL = 'Validator\Rules\Email';
+    const NUMBER    = 'Validator\Rules\Number';
+    const BOOLEAN   = 'Validator\Rules\Boolean';
+    const EMAIL     = 'Validator\Rules\Email';
+    const MAX       = 'Validator\Rules\Max';
 }
